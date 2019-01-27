98d664f70d8bbd4a024408ca7c50b0e936d3067d
suguloga <lcsugit@gmail.com>
2019-01-26 16:20:17 -0800

    Print multiple lines

--- snake.go
+++ snake.go
@@ -7,8 +7,13 @@ import (
 
 func main() {
     var forever bool = true
+    fmt.Printf("\033[2J")
+    fmt.Printf("\033[0;0H")
     for forever {
         t := time.Now()
-        fmt.Printf("\r %v", t)
+        fmt.Printf("\033[1m Hello: %v\n", t)
+        fmt.Printf("\033[1m Hello: %v\n", t)
+        fmt.Printf("\033[1m Hello: %v\n", t)
+        fmt.Printf("\033[0;0H")
     }
 }
