65cd6e8771f44b660a20530dbc567c9589879056
suguloga <lcsugit@gmail.com>
2019-01-26 16:37:56 -0800

    Hide cursor

--- snake.go
+++ snake.go
@@ -3,13 +3,31 @@ package main
 import (
     "fmt"
     "time"
+    "os"
+    "os/signal"
+    "syscall"
 )
 
+
 func main() {
-    var forever bool = true
+    // Hide cursor
+    fmt.Printf("\033[?25l")
+    // Clear screen
     fmt.Printf("\033[2J")
+    // Move cursor to top left
     fmt.Printf("\033[0;0H")
-    for forever {
+
+    // On exit, make sure cursor is turned back on
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    go func(){
+        <-c
+        fmt.Printf("\033[?25h")
+        os.Exit(1)
+    }()
+   
+    // Exit game code
+    for {
         t := time.Now()
         fmt.Printf("\033[1m Hello: %v\n", t)
         fmt.Printf("\033[1m Hello: %v\n", t)
