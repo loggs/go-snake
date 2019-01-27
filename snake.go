52e3ef96d2f717efe3a4a34697efd623fec78412
suguloga <lcsugit@gmail.com>
2019-01-27 09:19:23 -0800

    Format code

--- snake.go
+++ snake.go
@@ -1,85 +1,127 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/signal"
-    "syscall"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-func defaultBoard() [30][100]string {
-   var board [30][100]string
-    // Populate the board with 
-    for i := 0; i < 30; i++ {
-        for j := 0; j < 100; j++ {
-            board[i][j] = "\033[48;5;239m "
-        }
-    }
+type Game struct {
+	closed chan struct{}
+	snake  [3][2]int
+}
 
-    return board
+func (g *Game) Start() {
+
+	for {
+		select {
+		case <-g.closed:
+			return
+		default:
+			var board [30][100]string
+			board = defaultBoard()
+
+			// Replace board positions containing snake
+			// with white tiles
+			for _, pos := range g.snake {
+				board[pos[1]][pos[0]] = "\u001b[48;5;15m "
+			}
+
+			// Build the board
+			for _, row := range board {
+				fmt.Printf(join(row))
+			}
+
+			// Put the cursor back at 0,0
+			fmt.Printf("\033[0;0H")
+
+			// Move the snake forward
+			g.Move()
+
+			// Put delay in to slow render speed
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+
+}
+
+func (g *Game) Move() {
+	if g.snake[2][0] < 99 {
+
+		g.snake[0][0] = g.snake[0][0] + 1
+		g.snake[1][0] = g.snake[1][0] + 1
+		g.snake[2][0] = g.snake[2][0] + 1
+
+	}
+}
+
+func (g *Game) Stop() {
+	close(g.closed)
+}
+
+func join(arr [100]string) string {
+	// Join an array of strings with and empty string
+	s := ""
+	for _, str := range arr {
+		s = s + str
+	}
+
+	return s + "\n"
+}
+
+func defaultBoard() [30][100]string {
+
+	// Init the board, 30 * 100 pixels large
+	var board [30][100]string
+
+	// Populate the board with
+	for i := 0; i < 30; i++ {
+		for j := 0; j < 100; j++ {
+			board[i][j] = "\033[48;5;239m "
+		}
+	}
+
+	return board
 
 }
 
 func main() {
-    // Hide cursor
-    fmt.Printf("\033[?25l")
-    // Clear screen
-    fmt.Printf("\033[2J")
-    // Move cursor to top left
-    fmt.Printf("\033[0;0H")
+	// Create snake
+	var snake [3][2]int
+	snake[1][0] = 1
+	snake[2][0] = 2
 
-    // On exit, make sure cursor is turned back on
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-    go func(){
-        <-c
-        fmt.Printf("\033[2J")
-        fmt.Printf("\033[?25h")
-        os.Exit(1)
-    }()
+	game := &Game{
+		closed: make(chan struct{}),
+		snake:  snake,
+	}
 
-    var snake [3][2]int
-    snake[1][0] = 1
-    snake[2][0] = 2
+	// Hide cursor
+	fmt.Printf("\033[?25l")
+	// Clear screen
+	fmt.Printf("\033[2J")
+	// Move cursor to top left
+	fmt.Printf("\033[0;0H")
 
+	// On exit, make sure cursor is turned back on
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-    for {
+	go func() {
+		<-c
+		// Retrun cursor
+		fmt.Printf("\033[?25h")
+		// Clear screen
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		fmt.Printf("\033[H\033[2J")
+		game.Stop()
+	}()
 
-        var board[30][100]string
-        board = defaultBoard()
- 
-        // Replace board positions containing snake
-        // with white tiles
-        for _, pos := range snake {
-            board[pos[1]][pos[0]] = "\u001b[48;5;15m "
-        }
- 
-        // Build the board
-        for _, row := range board {
-            s := ""
-            for _, str := range row {
-                s = s + str
-            }
-            fmt.Printf(s + "\n")
-        }
+	game.Start()
 
-        fmt.Printf("\033[0;0H")
-
-        if snake[2][0] < 99 {
-
-            snake[0][0] = snake[0][0] + 1
-            snake[1][0] = snake[1][0] + 1
-            snake[2][0] = snake[2][0] + 1
-
-        }
-
-        // fmt.Printf("\033[1m\u001b[48;5;15m    \u001b[48;5;239m%v\n", strings.Repeat(" ", 96))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-        // fmt.Printf("\033[1m\u001b[48;5;239m%v\n", strings.Repeat(" ", 100))
-    }
 }
